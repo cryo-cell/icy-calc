097e29016f1d3421ee0e36879004163ded0a2ed4
Cruos Pluton <cryobabbi@gmail.com>
2022-05-25 03:49:11 -0400

    added current and previous operand functionality

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let display = document.getElementById('display');
+let current = document.getElementById('current-operand');
+
+let previous = document.getElementById('previous-operand');
+
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
@@ -6,11 +10,12 @@ buttons.map( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText) {
             case 'C':
-                display.innerText = '';
+                previous.innerText = '';
+                current.innerText = '';
             break;
             case '←':
-                if(display.innerText){
-                display.innerText = display.innerText.slice(0, -1);
+                if(current.innerText){
+                current.innerText = current.innerText.slice(0, -1);
                 }
             break;
             case '=':
@@ -39,14 +44,23 @@ buttons.map( button => {
 window.addEventListener("keydown", function(event) {
     let displayRegex = /^\d+|[*|+|-|/|.]/g;
     let result = event.key.match(displayRegex);
-    if (event.key === "c") {
-        display.innerText = '';}
+   
+    if ((event.key === "+" || event.key === "*" || event.key === "/" || event.key === "-") && previous.innerText === '') {
+        previous.innerText = current.innerText + event.key;
+        current.innerText = '';
+    }
     else if (event.key === "Enter") {
-        display.innerText = eval(display.innerText);}
+        current.innerText = eval(display.innerText);
+        previous.innerText = '';
+    }
     else if (event.key === "Backspace") {
-        display.innerText = display.innerText.slice(0, -1);} 
-    else if(result) {
-        display.innerText += `${event.key}`;
-        display.appendChild(display.innerText);} return false;
+        current.innerText = current.innerText.slice(0, -1);} 
+        else if (event.key === "c") {
+            previous.innerText ='';
+            current.innerText = '';
+        }   
+        else if(result) {
+        current.innerText += `${event.key}`;
+        display.appendChild(display.innerText);} return event.key;
   }, true);
   
